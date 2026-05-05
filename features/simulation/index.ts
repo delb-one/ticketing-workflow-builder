95fc1da06d478476d7960dce4d56e0be950a2781
delb-one <delbi92@gmail.com>
2026-05-05 14:19:29 +0200

    udpate import

--- features/simulation/index.ts
+++ features/simulation/index.ts
@@ -1,5 +1,5 @@
-export { default as SimulationPanel } from "./components/SimulationPanel";
+export { SimulationPanel } from "./components/SimulationPanel";
 export { AgentPanel } from "./components/AgentPanel";
 export { QueuePanel } from "./components/QueuePanel";
 export { TicketMonitor } from "./components/TicketMonitor";
-export { default as SimulationToolbar } from "./components/SimulationToolbar";
+export { TicketPanel } from "./components/TicketPanel";
