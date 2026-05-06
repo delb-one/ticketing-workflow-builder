1863252094ffd9aaeac6e72ca3ee2f41d712593b
delb-one <delbi92@gmail.com>
2026-05-06 10:01:12 +0200

    add auto scroll behaviour on log panel

--- features/simulation/components/SimulationPanel.tsx
+++ features/simulation/components/SimulationPanel.tsx
@@ -15,6 +15,7 @@ import {
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 import { CustomPanel } from "@/components/molecules/CustomPanel";
+import { useEffect, useRef, useState } from "react";
 
 const getEventIcon = (eventType: SimulationEvent["type"]) => {
   if (eventType === "workflow.started") return Play;
@@ -56,32 +57,36 @@ const getEventText = (event: SimulationEvent): string => {
 
 export function SimulationPanel() {
   const { simulationEvents } = useWorkflowStore();
-
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [simulationEvents]);
   return (
     <CustomPanel title="Log Panel" value="log-panel" icon={Terminal}>
-      <ScrollArea className="w-full ">
-        <div className="max-h-54">
-          <div className="space-y-1.5 font-mono text-sm">
-            {simulationEvents.length === 0 ? (
-              <div className="flex items-center gap-2 text-primary">
-                <Pause className="h-4 w-4" />
-                <span>Idle</span>
-              </div>
-            ) : (
-              simulationEvents.map((event, index) => {
-                const EventIcon = getEventIcon(event.type);
-                return (
-                  <div
-                    key={`${event.timestamp}-${event.type}-${index}`}
-                    className="flex items-center gap-2 text-primary"
-                  >
-                    <EventIcon className="h-4 w-4 shrink-0 text-primary" />
-                    <span>{getEventText(event)}</span>
-                  </div>
-                );
-              })
-            )}
-          </div>
+      <ScrollArea className="w-full">
+        <div className="space-y-1.5 font-mono text-sm p-2 max-h-56">
+          {simulationEvents.length === 0 ? (
+            <div className="flex justify-center items-center gap-2 text-primary">
+              <Pause className="h-4 w-4" />
+              <span>Idle</span>
+            </div>
+          ) : (
+            simulationEvents.map((event, index) => {
+              const EventIcon = getEventIcon(event.type);
+              return (
+                <div
+                  key={`${event.timestamp}-${event.type}-${index}`}
+                  className="flex items-center gap-2 text-primary"
+                >
+                  <EventIcon className="h-4 w-4 shrink-0 text-primary" />
+                  <span>{getEventText(event)}</span>
+                </div>
+              );
+            })
+          )}
+
+
+          <div ref={bottomRef} />
         </div>
       </ScrollArea>
     </CustomPanel>
